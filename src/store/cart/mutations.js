4a9bb98dd687cda2ef445a3f2a5d7f1cbdd58f00
Felix Anducho <felix.anducho74@gmail.com>
2022-02-23 19:56:53 -0600

    Add logic for adding and deleting items from cart

--- src/store/cart/mutations.js
+++ src/store/cart/mutations.js
@@ -1,5 +1,27 @@
 export default {
   addProductToCart(state, productData) {
-    return console.log(state, productData);
+    const productInCartIndex = state.cart.items.findIndex(
+      (ci) => ci.productId === productData.id
+    );
+    if (productInCartIndex >= 0) {
+      state.cart.items[productInCartIndex].qty++;
+    } else {
+      const newItem = { ...productData };
+      state.cart.items.push(newItem);
+    }
+    state.cart.qty++;
+    state.cart.total += productData.price;
+    console.log(state.cart);
+  },
+  removeProductFromCart(state, id) {
+    const productInCartIndex = state.cart.items.findIndex(
+      (itemInCart) => itemInCart.id === id
+    );
+    if (productInCartIndex >= 0) {
+      return;
+    }
+    const prodData = state.cart.items[productInCartIndex];
+    state.cart.items.splice(productInCartIndex, 1);
+    state.cart.total -= prodData.price;
   },
 };

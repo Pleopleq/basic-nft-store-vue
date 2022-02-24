ed1b2e48307cdbd54bd6fada061c4945951fc5f1
Felix Anducho <felix.anducho74@gmail.com>
2022-02-23 20:08:37 -0600

    Fix bugs in cart mutations

--- src/store/cart/mutations.js
+++ src/store/cart/mutations.js
@@ -11,17 +11,14 @@ export default {
     }
     state.cart.qty++;
     state.cart.total += productData.price;
-    console.log(state.cart);
   },
   removeProductFromCart(state, id) {
     const productInCartIndex = state.cart.items.findIndex(
       (itemInCart) => itemInCart.id === id
     );
-    if (productInCartIndex >= 0) {
-      return;
-    }
     const prodData = state.cart.items[productInCartIndex];
     state.cart.items.splice(productInCartIndex, 1);
+    state.cart.qty -= 1;
     state.cart.total -= prodData.price;
   },
 };

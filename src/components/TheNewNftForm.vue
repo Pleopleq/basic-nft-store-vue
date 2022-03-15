b8fe9fc1a3d2a72414a62fe042458ab84a4563de
Felix Anducho <felix.anducho74@gmail.com>
2022-03-14 20:51:44 -0600

    Add styles in form

--- src/components/TheNewNftForm.vue
+++ src/components/TheNewNftForm.vue
@@ -4,19 +4,27 @@
     <div class="form-container">
       <form action="">
         <div>
-          <label for="">Name</label>
+          <label for="name">Name</label>
         </div>
-        <div><input type="text" /></div>
+        <div><input type="text" class="form-input" /></div>
         <div>
-          <label for="">Image</label>
+          <label for="image">Image</label>
         </div>
-        <div><input type="text" /></div>
+        <div><input type="text" class="form-input" /></div>
         <div>
-          <label for="">Price</label>
+          <label for="price">Price</label>
         </div>
-        <div><input type="number" /></div>
-        <div><button>Submit</button></div>
+        <div><input type="text" class="form-input" /></div>
+        <div><button class="form-submit">Submit</button></div>
       </form>
+    </div>
+    <div class="alert-container">
+      <div>
+        <p>Just use number inside the price input.</p>
+      </div>
+      <div>
+        <p>Please fill all the inputs.</p>
+      </div>
     </div>
   </section>
 </template>
@@ -29,12 +37,26 @@ export default {};
 .form-view {
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   margin: 3rem;
+  height: 100vh;
 }
 
-.form-container {
-  height: 100vh;
+.form-input {
+  margin: 0.5rem 0 0.7rem 0;
+  padding: 0.3rem;
+}
+
+.form-submit {
+  width: 100%;
+}
+
+.alert-container {
+  text-align: center;
+  background-color: #e89090;
+  color: #ba2626;
+  padding: 0.5rem;
+  margin-top: 1rem;
+  border-radius: 24px;
 }
 </style>
